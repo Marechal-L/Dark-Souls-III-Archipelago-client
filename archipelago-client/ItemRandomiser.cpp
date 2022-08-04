f9361e38dd00728a774520eb74e04619f58459df
Ludovic Marechal <marechal-l@gmx.com>
2022-08-04 20:13:21 +0200

    Remove the gesture specific case

--- archipelago-client/ItemRandomiser.cpp
+++ archipelago-client/ItemRandomiser.cpp
@@ -34,10 +34,9 @@ VOID CItemRandomiser::RandomiseItem(UINT_PTR qWorldChrMan, UINT_PTR pItemBuffer,
 		dItemQuantity = *(int*)(pItemBuffer + 0x04);
 		dItemDurability = *(int*)(pItemBuffer + 0x08);
 
-		//Specific case for the gesture
-		if (dItemID == 0x40002346) {
-			enablePathOfTheDragon = true;
-		}
+#ifdef DEBUG
+		printf("itemId : %d\n", dItemID);
+#endif // DEBUG
 
 		//Make some checks about the item picked by the player
 		int serverLocationIndex = -1;
